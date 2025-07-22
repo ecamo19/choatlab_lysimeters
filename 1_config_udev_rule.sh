53d8b294942320c25f77bd931b8a2e4b1a6564e2
ecamo19 <erickcamo19@gmail.com>
2025-07-22 23:15:52 +1000

    Path updated

--- 1_config_udev_rule.sh
+++ 1_config_udev_rule.sh
@@ -3,4 +3,4 @@
 # Modify the /home/choatlab if required
 # Run code by using bash 1_config_udev_rule.sh 
 
-mv /home/choatlab/lysimeters_code/src/99-libphidget22.rules /etc/udev/rules.d/99-libphidget22.rules & udevadm control --reload-rules & udevadm trigger
+mv /home/choatlab/choatlab_lysimeters/src/99-libphidget22.rules /etc/udev/rules.d/99-libphidget22.rules & udevadm control --reload-rules & udevadm trigger
