6f2b80545c6f95f654272c5158e7aabec5640be5
ecamo19 <erickcamo19@gmail.com>
2025-08-15 18:08:49 +1000

    Instructions updated

--- instructions_for_crontab_config.sh
+++ instructions_for_crontab_config.sh
@@ -7,33 +7,42 @@
 
 ## 1) Change the persimission of rclone file ------------------------------------
 
+```bash
 # chmod +x ~/choatlab_lysimeters/2_upload_lysimeter_data.sh
+```
 
 ## 2) Configure rclone  ---------------------------------------------------------
 
+```bash
 # rclone config 
+```
 
-# Remember to input N on question: 
+## Remember to input N on question:
+
  # Use web browser to automatically authenticate rclone with remote?
  # * Say Y if the machine running rclone has a web browser you can use
  # * Say N if running rclone on a (remote) machine without web browser access
 
+## Remember to name the folder as onedrive
+
 # Check rclone is working
 # The code should list some files in the OneDrive 
 
+```bash
 # rclone ls onedrive:/ | tail
+```
 
 ## 3) Configure crontab ---------------------------------------------------------
 
-# List current user's cron jobs
-
-# crontab -l
-
 # Configure crontab
 
+```bash
 # crontab -e
+```
 
 ## Copy and paste one the following: 
+
+## Remember to edit the 2_upload_lysimeter_data.sh with the correct phidget 
 
 # Run upload code every minute
 # * * * * * /home/choatlab/choatlab_lysimiters/2_upload_lysimeter_data.sh >> /home/choatlab/upload.log 2>&1
@@ -46,3 +55,9 @@
 
 # Run upload code every day at midnight 
 # 0 0 * * * /home/choatlab/choatlab_lysimiters/2_upload_lysimeter_data.sh >> /home/choatlab/upload.log 2>&1
+
+# List current user's cron jobs
+
+```bash
+# crontab -l
+```
